6ed162522daea9be0895702725d4d814246eb93c
mattdelta2 <matt.wierenga@outlook.com>
2021-11-05 08:20:27 +0200

    Added Abstract class Item

---
+++ Item.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    abstract class Item
+    {
+        public Item(int _X, int _Y)
+        {
+
+        }
+        
+            private int x;
+        public int _X
+        {
+            get { return x; }
+            set { _X = value; }
+
+        }
+        private int y;
+        public int _Y
+        {
+            get { return y; }
+            set { _Y = value; }
+
+        }
+    }
+}
+
+

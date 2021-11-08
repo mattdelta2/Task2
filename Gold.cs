92231cd2ba059779c0ed2136b15f6d0f88b8722b
mattdelta2 <matt.wierenga@outlook.com>
2021-11-08 10:58:20 +0200

    Added Gold class

---
+++ Gold.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Gold
+    {
+
+        private int GOLD;
+
+        public int gold
+        {
+            get { return GOLD; }
+            set { gold = value; }
+        }
+
+        protected Random RANDOM_NUMBER_GENERATOR = new Random();
+
+    }
+}
